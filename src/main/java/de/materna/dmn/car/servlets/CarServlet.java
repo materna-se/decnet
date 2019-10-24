ba10e8ee62636c69df686d97c2e02cc4741230d9
Jonas Tamimi <oas@users.noreply.github.com>
2019-10-24 11:30:04 +0200

    Added /store endpoint to allow the import of decision models

--- src/main/java/de/materna/dmn/car/servlets/CarServlet.java
+++ src/main/java/de/materna/dmn/car/servlets/CarServlet.java
@@ -3,50 +3,40 @@ package de.materna.dmn.car.servlets;
 
 import de.materna.jdec.DecisionSession;
 import de.materna.jdec.helpers.SerializationHelper;
-import org.apache.commons.io.IOUtils;
 
-import javax.ws.rs.Consumes;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.Response;
-import java.io.IOException;
-import java.io.InputStream;
-import java.nio.charset.StandardCharsets;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
-import java.util.Base64;
 import java.util.Map;
 
 @Path("")
 public class CarServlet {
-	private DecisionSession decisionSession;
+	private DecisionSession decisionSession = new DecisionSession();
 
-	public CarServlet() throws IOException {
-		// The model "model.dmn" is loaded from the "resources" folder.
-		InputStream inputStream = Thread.currentThread().getContextClassLoader().getResourceAsStream("/model.dmn");
-		String decision = IOUtils.toString(inputStream, StandardCharsets.UTF_8);
+	@PUT
+	@Path("/store")
+	@Consumes("application/xml")
+	public Response importModel(String model) {
+		System.out.println("importModel: " + model);
 
-		// The model is imported. During import, the Drools instance, among other things, is initialized.
-		decisionSession = new DecisionSession();
-		decisionSession.importModel(decision);
+		// The model is imported.
+		// During import, the Drools instance, among other things, is initialized.
+		decisionSession.importModel(model);
+
+		return Response.status(Response.Status.NO_CONTENT).build();
 	}
 
 	@POST
-	@Path("")
+	@Path("/executor")
 	@Consumes("application/json")
 	@Produces("application/json")
-	public Response getInputs(String input) throws NoSuchAlgorithmException {
+	public Response getInputs(String input) {
+		System.out.println("getInputs: " + input);
+
 		// executeModel serializes the inputs automatically and passes them on to the Drools engine.
 		// When the output is calculated, it is returned as a Map<String, Object> and can be used freely.
 		Map<String, Object> calculatedOutput = decisionSession.executeModel(input);
 
-		return Response.status(Response.Status.OK).header("Cache-Control", "public, max-age=5").entity(SerializationHelper.getInstance().toJSON(calculatedOutput)).build();
-	}
-
-	private String calculateHash(String input) throws NoSuchAlgorithmException {
-		MessageDigest messageDigest = MessageDigest.getInstance("SHA-256");
-		messageDigest.update(input.getBytes());
-		return Base64.getEncoder().encodeToString(messageDigest.digest());
+		String output = SerializationHelper.getInstance().toJSON(calculatedOutput);
+		return Response.status(Response.Status.OK).header("Cache-Control", "public, max-age=5").entity(output).build();
 	}
 }
